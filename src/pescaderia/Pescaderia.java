b1659bda4de4d35b84397a181f8428709de863d2
rainbown1 <milcacelestenavadedios@gmail.com>
2025-08-21 16:16:36 -0600

    first commit

---
+++ src/pescaderia/Pescaderia.java
@@ -0,0 +1,26 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package pescaderia;
+
+import View.Login;
+
+/**
+ *
+ * @author LENOVO
+ */
+public class Pescaderia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        Login l = new Login();
+        l.setVisible(true);
+        l.pack();
+        l.setLocationRelativeTo(null);
+    }
+    
+}
